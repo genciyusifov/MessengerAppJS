1426706d836bc6dc39b64c1761b681510c43bc23
Gənci <yusifovgenci91@gmail.com>
2023-06-07 22:02:39 +0400

    Initial commit

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:3000";
+
+
+const API_URL = "http://localhost:3000"
 // function getRandomNumberBetween(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min);
 // }
